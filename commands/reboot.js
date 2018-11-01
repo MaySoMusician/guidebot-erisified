6c694a161c7339941c55e55a44b7fab9943e3f0e
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:31:53 +0900

    Erisify commands/reboot.js

--- commands/reboot.js
+++ commands/reboot.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  await message.reply("Bot is shutting down.");
+  await client.replyMessage(message, "Bot is shutting down.");
   client.commands.forEach( async cmd => {
     await client.unloadCommand(cmd);
   });
