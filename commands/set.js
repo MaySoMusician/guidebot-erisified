f3abf404468c5780cddae8ded411c92c31cd6455
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:34:30 +0900

    Erisify commands/set.js
    Warning for deletion is disable because we haven't implemented custom awaitReply
    which uses the message collector, not ported from Discord.js

--- commands/set.js
+++ commands/set.js
@@ -15,64 +15,70 @@ exports.run = async (client, message, [action, key, ...value], level) => { // es
   // Retrieve current guild settings (merged) and overrides only.
   const settings = message.settings;
   const defaults = client.config.defaultSettings;
-  const overrides = client.settings.get(message.guild.id);
-  if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
+  const overrides = client.settings.get(message.channel.guild.id);
+  if (!client.settings.has(message.channel.guild.id)) client.settings.set(message.channel.guild.id, {});
   
   // Edit an existing key value
   if (action === "edit") {
     // User must specify a key.
-    if (!key) return message.reply("Please specify a key to edit");
+    if (!key) return client.replyMessage(message, "Please specify a key to edit");
     // User must specify a key that actually exists!
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
+    if (!defaults[key]) return client.replyMessage(message, "This key does not exist in the settings");
     const joinedValue = value.join(" ");
     // User must specify a value to change.
-    if (joinedValue.length < 1) return message.reply("Please specify a new value");
+    if (joinedValue.length < 1) return client.replyMessage(message, "Please specify a new value");
     // User must specify a different value than the current one.
-    if (joinedValue === settings[key]) return message.reply("This setting already has that value!");
+    if (joinedValue === settings[key]) return client.replyMessage(message, "This setting already has that value!");
     
     // If the guild does not have any overrides, initialize it.
-    if (!client.settings.has(message.guild.id)) client.settings.set(message.guild.id, {});
+    if (!client.settings.has(message.channel.guild.id)) client.settings.set(message.channel.guild.id, {});
 
     // Modify the guild overrides directly.
-    client.settings.set(message.guild.id, joinedValue, key);
+    client.settings.set(message.channel.guild.id, joinedValue, key);
 
     // Confirm everything is fine!
-    message.reply(`${key} successfully edited to ${joinedValue}`);
+    client.replyMessage(message, `${key} successfully edited to ${joinedValue}`);
   } else
   
   // Resets a key to the default value
   if (action === "del" || action === "reset") {
-    if (!key) return message.reply("Please specify a key to reset.");
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
-    if (!overrides[key]) return message.reply("This key does not have an override and is already using defaults.");
+    if (!key) return client.replyMessage(message, "Please specify a key to reset.");
+    if (!defaults[key]) return client.replyMessage(message, "This key does not exist in the settings");
+    if (!overrides[key]) return client.replyMessage(message, "This key does not have an override and is already using defaults.");
     
     // Good demonstration of the custom awaitReply method in `./modules/functions.js` !
-    const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?`);
+    // HOWEVER, we haven't get that one yet for some reasons.
+    // So the setting will be deleted without ANY warnings
+    client.replyMessage(message, `By right, you'll be asked if you surely want to reset ${key} to the default value, with a wonderful custom *reply* function. HOWEVER we haven't get that yet for some reasons. In short, the setting will be reset without ANY warning.\nBut don't worry. We've kept a backup here: **${settings[key]}**`);
+    client.settings.delete(message.channel.guild.id, key);
+    client.replyMessage(message, `${key} was successfully reset to default.`);
+
+    /*const response = await client.awaitReply(message, `Are you sure you want to reset ${key} to the default value?`);
 
     // If they respond with y or yes, continue.
     if (["y", "yes"].includes(response.toLowerCase())) {
       // We delete the `key` here.
-      client.settings.delete(message.guild.id, key);
-      message.reply(`${key} was successfully reset to default.`);
+      client.settings.delete(message.channel.guild.id, key);
+      client.replyMessage(message, `${key} was successfully reset to default.`);
     } else
     // If they respond with n or no, we inform them that the action has been cancelled.
     if (["n","no","cancel"].includes(response)) {
-      message.reply(`Your setting for \`${key}\` remains at \`${settings[key]}\``);
-    }
+      client.replyMessage(message, `Your setting for \`${key}\` remains at \`${settings[key]}\``);
+    }*/
   } else
   
   if (action === "get") {
-    if (!key) return message.reply("Please specify a key to view");
-    if (!defaults[key]) return message.reply("This key does not exist in the settings");
+    if (!key) return client.replyMessage(message, "Please specify a key to view");
+    if (!defaults[key]) return client.replyMessage(message, "This key does not exist in the settings");
     const isDefault = !overrides[key] ? "\nThis is the default global default value." : "";
-    message.reply(`The value of ${key} is currently ${settings[key]}${isDefault}`);
+    client.replyMessage(message, `The value of ${key} is currently ${settings[key]}${isDefault}`);
   } else {
     // Otherwise, the default action is to return the whole configuration;
     const array = [];
     Object.entries(settings).forEach(([key, value]) => {
       array.push(`${key}${" ".repeat(20 - key.length)}::  ${value}`); 
     });
-    await message.channel.send(`= Current Guild Settings =\n${array.join("\n")}`, {code: "asciidoc"});
+    await client.createMessageExtended(message.channel.id, `= Current Guild Settings =\n${array.join("\n")}`, null, {code: "asciidoc"});
   }
 };
 
