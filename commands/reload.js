64948d0408e3a115966a278cbe9990acf3ea3974
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:32:07 +0900

    Erifisy commands/reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,13 +1,13 @@
 exports.run = async (client, message, args, level) => {// eslint-disable-line no-unused-vars
-  if (!args || args.length < 1) return message.reply("Must provide a command to reload. Derp.");
+  if (!args || args.length < 1) return client.replyMessage(message, "Must provide a command to reload. Derp.");
 
   let response = await client.unloadCommand(args[0]);
-  if (response) return message.reply(`Error Unloading: ${response}`);
+  if (response) return client.replyMessage(message, `Error Unloading: ${response}`);
 
   response = client.loadCommand(args[0]);
-  if (response) return message.reply(`Error Loading: ${response}`);
+  if (response) return client.replyMessage(message, `Error Loading: ${response}`);
 
-  message.reply(`The command \`${args[0]}\` has been reloaded`);
+  client.replyMessage(message, `The command \`${args[0]}\` has been reloaded`);
 };
 
 exports.conf = {
