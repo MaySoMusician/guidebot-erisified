7bff772debd78b827a2a2ed0a5f7ef578227fd9f
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:39:45 +0900

    Erisify events/ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async client => {
   // Log that the bot is online.
-  client.logger.log(`${client.user.tag}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
+  client.logger.log(`${client.user.username}#${client.user.discriminator}, ready to serve ${client.users.size} users in ${client.guilds.size} servers.`, "ready");
 
   // Make the bot "play the game" which is the help command with default prefix.
-  client.user.setActivity(`${client.config.defaultSettings.prefix}help`, {type: "PLAYING"});
+  client.editStatus("online", {name: `${client.config.defaultSettings.prefix}help`, type: 0});
 };
