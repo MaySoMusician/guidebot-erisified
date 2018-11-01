d7dffe0e9011d25cdd71b65f6b5eeb5c539ed64c
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:37:57 +0900

    Erisify events/guildCreate.js
    Use a troublesome way to get data of the owner,
    which I don't know works fine

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,5 +1,8 @@
 // This event executes when a new guild (server) is joined.
 
-module.exports = (client, guild) => {
-  client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${guild.owner.user.tag} (${guild.owner.user.id})`);
+module.exports = async (client, guild) => {
+  guild.fetchAllMembers();
+  await client.wait(2000);
+  const owner = guild.members.get(guild.ownerID);
+  client.logger.cmd(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot. Owner: ${owner.username}#${owner.discriminator} (${guild.ownerID})`);
 };
