44d7378ed0fd12b72f3a8e756cb8aa85bb2e7ede
MaySoMusician <maysomusician@gmail.com>
2018-11-01 23:31:42 +0900

    Erisify commands/ping.js
    Use a troublesome way to get API Latency

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,10 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send("Ping?");
-  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  const msg = await message.channel.createMessage("Ping?");
+  msg.edit(`Pong! Latency is ${msg.timestamp - message.timestamp}ms.`);
+  if (message.channel.guild) {
+    const currentShard = client.shards.get(client.guildShardMap[message.channel.guild.id]);
+    msg.edit(`Pong! Latency is ${msg.timestamp - message.timestamp}ms. API Latency is ${Math.round(currentShard.latency)}ms`);
+  }
 };
 
 exports.conf = {
